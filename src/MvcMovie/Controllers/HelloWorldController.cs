fe09e6f0b530638b51df5aa6d1d76118cdabbaff
Angelo Gramatica <angelo.gramatica@bluewin.ch>
2016-06-03 22:09:10 +0200

    w

--- src/MvcMovie/Controllers/HelloWorldController.cs
+++ src/MvcMovie/Controllers/HelloWorldController.cs
@@ -8,7 +8,6 @@ namespace MvcMovie.Controllers
     {
         // 
         // GET: /HelloWorld/
-
         public IActionResult Index()
         {
             return View();
@@ -16,11 +15,12 @@ namespace MvcMovie.Controllers
 
         // 
         // GET: /HelloWorld/Welcome/ 
-
-        public string Welcome(string name, int numTimes = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode(
-                "Hello " + name + ", NumTimes is: " + numTimes);
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
